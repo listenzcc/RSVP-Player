4972f5550360f4a6119598445545bf8d522e64b9
listenzcc <listenzcc@mail.bnu.edu.cn>
2022-06-02 09:05:20 +0800

    Main-loop uses the new controller

--- RSVP-Player/singleInstance/main_loop.py
+++ RSVP-Player/singleInstance/main_loop.py
@@ -1,50 +1,28 @@
 # %%
 from .logger import LOGGER
 from .loop_manager import LOOP_MANAGER
+
+from .toolbox import Controller
+
 from .constants import *
 
 # %%
 
 
-def draw_controllers():
-    width = 1
-    color = WHITE
-    background = None
-    antialias = True
+controllers = dict(
+    CAPTURE=['__CAPTURE__', -1],
+    RSVP=['__RSVP__', -1],
+)
 
-    top = int(CFG['screen']['height']) / 10
-    _top = 20
-    left = int(CFG['screen']['width']) / 2
-
-    controllers = dict(
-        CAPTURE=['__CAPTURE__', -1],
-        RSVP=['__RSVP__', -1],
-    )
-
-    for j, cmd in enumerate(controllers):
-        string = controllers[cmd][0]
-        text = FONT.render(string, antialias, color, background)
-        rect = text.get_rect()
-        rect.height *= 1.1
-        rect.center = (left, top)
-        top += _top + rect.height
-
-        SCREEN.fill(BLACK, rect)
-        SCREEN.blit(text, rect)
-        pygame.draw.rect(SCREEN, color, rect, width=width)
-
-        controllers[cmd][1] = rect
-
-    return controllers
+controller = Controller(controllers, title='Main')
 
 
 # %%
 def main_loop():
     LOGGER.info('Main loop started')
-    controllers = draw_controllers()
     while True:
         SCREEN.fill(BLACK)
-        draw_controllers()
+        controller.draw()
         pygame.display.flip()
 
         if not LOOP_MANAGER.get() == 'MAIN':
@@ -55,14 +33,12 @@ def main_loop():
             if event.type == pygame.QUIT:
                 QUIT_PYGAME()
 
-            if event.type == pygame.MOUSEBUTTONUP:
-                for cmd in controllers:
-                    if controllers[cmd][1].contains(event.pos, (1, 1)):
-                        LOGGER.info('Event: MouseButtonUp: {}'.format(cmd))
-                        if cmd == 'CAPTURE':
-                            LOOP_MANAGER.set('CAPTURE')
+            cmd = controller.check(event)
 
-                        if cmd == 'RSVP':
-                            LOOP_MANAGER.set('RSVP')
+            if cmd == 'CAPTURE':
+                LOOP_MANAGER.set('CAPTURE')
+
+            if cmd == 'RSVP':
+                LOOP_MANAGER.set('RSVP')
 
 # %%
